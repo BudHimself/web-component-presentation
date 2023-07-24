2798f7f986cb55ea14de70f330fc0efaf4fb7836
budhimself <francois.henry@talan.com>
2023-07-24 13:57:45 +0200

    3 sabers work with web component

--- components/saber.js
+++ components/saber.js
@@ -4,28 +4,49 @@ class Saber extends HTMLElement {
         const opponentName = this.getAttribute("opponent-name")
 
         const template = document.querySelector("#saber")
-
-        const saber = document.createElement("div")
-        saber.setAttribute("id", `saber-${opponentName}`)
-        saber.classList.add("saber")
-
         const templateClone = template.cloneNode(true)
+
         const opponentImg = templateClone.content.querySelector("img.opponent-img")
         opponentImg.setAttribute("src", `img/${opponentName}.png`)
 
         const templateContent = templateClone.content.querySelector(".opponent")
-        templateContent.appendChild(saber)
+
+        const saberLight = this.createSaberLight(opponentName)
+        templateContent.appendChild(saberLight)
 
         const style = this.getStyle()
 
         this.appendChild(templateContent)
         this.appendChild(style)
+        this.addOpeningEvent()
+    }
+
+    addOpeningEvent() {
+        this.addEventListener('click', (event) => {
+            const open = "saber-open"
+            const parent = event.target.parentNode
+            const elem = parent.querySelector(".saber-light")
+
+            if (elem.classList.contains(open)) {
+                elem.classList.remove(open)
+            } else {
+                elem.classList.add(open)
+            }
+        })
+    }
+
+    createSaberLight(opponentName) {
+        const saberLight = document.createElement("div")
+        saberLight.setAttribute("id", `saber-${opponentName}`)
+        saberLight.classList.add("saber-light")
+
+        return saberLight
     }
 
     getStyle() {
         const style = document.createElement("style")
         style.textContent = `
-            .saber {
+            .saber-light {
                 position: absolute;
                 bottom: 270px;
                 height: 0;
