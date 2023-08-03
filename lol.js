6eef146b0833c253330ffbf7127f0c209b0ddd7c
budhimself <francois.henry@talan.com>
2023-08-03 12:03:36 +0200

    prez final

--- lol.js
+++
@@ -1,9 +0,0 @@
-const style = document.createElement("style")
-
-style.textContent = `
-#saber-angular {
-    background-color: yellow;
-    box-shadow: 0 0 5px 5px #ff0;
-}
-`
-document.querySelector("body").appendChild(style)
